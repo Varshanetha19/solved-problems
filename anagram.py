f5cfdf1343fa7988b26928e6e4f0958276774ae2
Varshanetha19 <163376845+Varshanetha19@users.noreply.github.com>
2024-04-01 23:28:39 +0530

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,10 @@
+x=input('enter the first string').lower()
+y=input('enter the second sring').lower()
+k=[x[i] for i in range(0,len(x))]
+k.sort() 
+l=[y[i] for i in range(0,len(y))]
+l.sort()
+if (k==l):
+    print('TRUE')
+else:
+    print('FLASE')
